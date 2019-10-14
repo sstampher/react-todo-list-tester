71b42ed09fd69e9612a82b06fd8e0e7d572be1ba
sstampher <stephanierstampher@gmail.com>
2019-10-14 14:45:59 -0700

    have everything working except for form clear on submit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import { TodoItems } from './TodoItems'
+// import React, { useState } from 'react'
+import React, { Component } from 'react';
+import { TodoItems } from './TodoItems';
 import './TodoList.css'
 
-export const TodoList = () => {
+export class TodoList extends Component {
   // The schema for a todo item expects a key and a text field
   // You can see it being used in TodoItems.js
   // eg: { key: 'someUniqueKey', text: 'The Text of the Item' }
@@ -13,27 +14,48 @@ export const TodoList = () => {
 
   // const [text, setText] = useState('')
   // const [items, setItems] = useState([])
-
-  const handleInput = e => {
-    // handle onChange of the input field
+  constructor(){
+    super();
+    this.state={
+      items: [],
+      text: {},
+      value: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
-  const handleSubmit = e => {
-    // handle onSubmit of the form
+ handleInput (e){
+    this.setState({
+      text: {'text':e.target.value, 'key':e.target.value}
+    })
   }
 
-  const deleteItem = key => {
-    // handle deleting when a task is clicked
+ handleSubmit = e => {
+   e.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.text]
+    })
+    console.log(this.text)
+    
   }
 
-  return (
+ deleteItem = key => {
+  this.setState({
+    items: this.state.items.filter(item => item.key !== key)
+  })
+  }
+
+  render() {
+    return (
     <div className="todos">
       <div className="header">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <input
             placeholder="enter task"
-            onChange={handleInput}
-            value={text}
+            onChange={this.handleInput}
+            value={this.text}
             className="new-task"
             data-testid="new-task"
             autoFocus
@@ -41,7 +63,8 @@ export const TodoList = () => {
           <button type="submit">add</button>
         </form>
       </div>
-      <TodoItems items={items} onDelete={deleteItem} />
+      <TodoItems items={this.state.items} onDelete={this.deleteItem} />
     </div>
-  )
+    )
+  }
 }
